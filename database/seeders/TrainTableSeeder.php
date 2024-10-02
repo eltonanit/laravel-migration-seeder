f7c5e4a98adbf7507ff30ac27e7d569a3952a6e6
eltonanit <elton.ani@gmail.com>
2024-10-02 15:41:12 +0200

    [FIX] Fixed bugs

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -4,9 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use faker\Generator as Faker;
+use Faker\Generator as Faker;
 
-use App\Models\Trains;
+use App\Models\Train;
 
 class TrainTableSeeder extends Seeder
 {
@@ -25,6 +25,7 @@ class TrainTableSeeder extends Seeder
             $train->arrival_station = $faker->randomElement(['Parigi','Vienna', 'Lecce']);
             $train->departure_time = $faker->dateTimeBetween('-1 day', '+1 day');
             $train->arrival_time = $faker->dateTimeBetween('+1 day', '+2 day');
+             $train->train_wagons = 'AB'.$faker->randomDigitNotNull();
             $train->train_code = 'AB'.$faker->randomNumber(4, false);
             $train->on_time = $faker->boolean();
             $train->cancelled = $faker->boolean();
